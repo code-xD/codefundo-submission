7ff4838ffe57d07c44b69ff741a8920366cce4f8
Shivansh Anand <shivansh586@gmail.com>
2019-08-21 09:42:33 +0530

    changes

--- regapi/urls.py
+++ regapi/urls.py
@@ -16,5 +16,4 @@ urlpatterns = [
     path('adduser/', AddUserView, name='add-user-view')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
